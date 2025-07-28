8b245a0efd86aae062dbb35ca77956d8e4ed4677
Barbara <barbara.joebstl@mercedes-benz.com>
2025-07-28 16:00:53 +0100

    update main, add test split and random forest

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -2,9 +2,12 @@ import pandas as pd
 import numpy as np
 import scipy.stats
 
+# if only this step should be executed
+# run with  iterm
+  
+
 
 def test_column_names(data):
-
     expected_colums = [
         "id",
         "name",
@@ -31,10 +34,9 @@ def test_column_names(data):
 
 
 def test_neighborhood_names(data):
-
     known_names = ["Bronx", "Brooklyn", "Manhattan", "Queens", "Staten Island"]
 
-    neigh = set(data['neighbourhood_group'].unique())
+    neigh = set(data["neighbourhood_group"].unique())
 
     # Unordered check
     assert set(known_names) == set(neigh)
@@ -44,7 +46,7 @@ def test_proper_boundaries(data: pd.DataFrame):
     """
     Test proper longitude and latitude boundaries for properties in and around NYC
     """
-    idx = data['longitude'].between(-74.25, -73.50) & data['latitude'].between(40.5, 41.2)
+    idx = data["longitude"].between(-74.25, -73.50) & data["latitude"].between(40.5, 41.2)
 
     assert np.sum(~idx) == 0
 
@@ -54,12 +56,20 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     Apply a threshold on the KL divergence to detect if the distribution of the new data is
     significantly different than that of the reference dataset
     """
-    dist1 = data['neighbourhood_group'].value_counts().sort_index()
-    dist2 = ref_data['neighbourhood_group'].value_counts().sort_index()
+    dist1 = data["neighbourhood_group"].value_counts().sort_index()
+    dist2 = ref_data["neighbourhood_group"].value_counts().sort_index()
 
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data):
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data, min_price, max_price):
+    """
+    Test that all prices in the dataset fall within the specified range.
+    """
+    if not data["price"].between(min_price, max_price).all():
+        out_of_range = data[~data["price"].between(min_price, max_price)]
+        raise ValueError(f"Some prices fall outside the range {min_price}–{max_price}:\n{out_of_range[['price']]}")
